0ce5d3df04d0822d0933c4a264dc1d05b555ec75
devPisa <cesar.pisa@alunos.bpkedu.com.br>
2025-06-17 11:56:04 -0300

    Feat: Implementation configuration database

---
+++ translation-api/src/infrastructure/database/prismaTranslationRepository.js
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+import { TranslationRepository } from "@/application/repositories/translationRepository";
+import { Translation } from "@/domain/translation";
+
+const prisma = new PrismaClient();
+
+export class PrismaTranslationRepository extends TranslationRepository{
+    async save(translation){
+        await prisma.translation.create({
+            data:{
+                id: translation.id,
+                originalText: translation.originalText,
+                targetLanguage:translation.targetLanguage,
+                status:translation.status,
+            },
+        });
+    }
+
+    async findById(id){
+        const result = await prisma.translation.findUnique({
+            where: { id }
+        });
+        if (!result) return null;
+        return new Translation(result);
+    }
+
+    async update(translation){
+        const dataToUpdate = {
+            status: translation.status
+        };
+        if(translation.translatedText) dataToUpdate.translatedText = translation.translatedText;
+        if(translation.error) dataToUpdate.error = translation.error;
+
+        await prisma.translation.update({
+            where: { id: translation.id },
+            data: dataToUpdate,
+        });
+    }
+}
