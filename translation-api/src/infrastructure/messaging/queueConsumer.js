53e39201c74250563835135576525ebe3aaa7fc5
devPisa <cesar.pisa@alunos.bpkedu.com.br>
2025-06-17 11:56:31 -0300

    Feat: Implementation comunication

---
+++ translation-api/src/infrastructure/messaging/queueConsumer.js
@@ -0,0 +1,30 @@
+import { amqp } from 'amqplib';
+
+export class QueueConsumer {
+    constructor(amqpUrl, processResultUseCase){
+        this.amqpUrl = amqpUrl;
+        this.processResultUseCase = processResultUseCase;
+        this.queueTranslate = 'q.translation.results';
+    }
+
+    async start(){
+        console.log('... API: Ouvinte de resultados esperando por mensagens.');
+        try{
+            const connection = await amqp.connect(this.amqpUrl);
+            const channel = await connection.createChannel();
+            await channel.assertQueue(this.queueTranslate, { durable: true });
+
+            channel.consume(this,this.queueTranslate, async(msg) => {
+                if(msg !== null){
+                    const message = JSON.parse(msg.content.toString());
+                    console.log(`[📥] API: Resultado recebido para ${message.requestId}`);
+
+                    await this.processResultUseCase.execute(message);
+                    channel.ack(msg);
+                }
+            }, { noAck: false });
+        } catch (error){
+            console.error(`❌ API: Erro no ouvinte de resultados:`, error);
+        }
+    }
+}
